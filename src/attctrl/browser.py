e1aa4124d3c84685fc6f90ae76b5efd82daf1b47
nickosh <nikolay.shishov@gmail.com>
2024-08-07 12:47:25 +0900

    Browser logic WIP

--- src/attctrl/browser.py
+++ src/attctrl/browser.py
@@ -1,26 +1,69 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, expect, sync_playwright
 
 from attctrl.config import Config
+from attctrl.logger import new_logger
+
+logger = new_logger(__name__, log_file="debug.log")
 
 
-class LoginPageSelectors:
-    username_input = "input[placeholder='Email address or mobile number']"
-    password_input = "input[placeholder='Enter password']"
-    next_button = "button:has-text('Next')"
+class BasicPage:
+    def __init__(self, page: Page) -> None:
+        self.page = page
+
+    def __call__(self) -> Page:
+        return self.page
 
 
-class DashboardPageSelectors:
-    url = "**/dashboard"
+class LoginPage(BasicPage):
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+        self.username_input = page.locator("input[placeholder='Email address or mobile number']")
+        self.password_input = page.locator("input[placeholder='Enter password']")
+        self.next_button = page.locator("button#nextbtn:has-text('Next')")
+        self.understand_button = page.locator("button#continue_button:has-text('I Understand')")
+        self.daily_limit = page.locator("text=You've reached your daily sign-in limit.")
+
+    def is_daily_limit_warning(self) -> bool:
+        try:
+            expect(self.understand_button).to_be_visible()
+            return True
+        except Exception:
+            return False
+
+    def is_daily_limit_reached(self) -> bool:
+        try:
+            expect(self.daily_limit).to_be_visible()
+            return True
+        except Exception:
+            return False
+
+
+class DashboardPage:
+    def __init__(self, page: Page) -> None:
+        self.page = page
+        self.url = "**/dashboard"
+        self.iframe = "iframe#peopleLoadFrame"
+        self.atte_header = "h3.dash-title[title='Attendance']"
+        self.time_span = "span#ZPAtt_Dash_CurrTime"
+        self.att_button = self.page.frame_locator(self.iframe).locator("button.out.chlodIng")
+
+    def wait_for_loading(self):
+        self.page.wait_for_url(self.url)
 
 
 class BrowserControl:
     def __init__(self, url: str = Config.ZOHO_LOGIN_LINK) -> None:
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.chromium.launch(headless=not Config.DEBUG)
+        self.context = self.browser.new_context()
         self.page = self.browser.new_page()
+        self.login_pg = LoginPage(self.page)
+        self.dashboard_pg = DashboardPage(self.page)
+
         self.page.goto(url)
 
     def teardown(self):
+        self.context.close()
         self.browser.close()
         self.playwright.stop()
 
@@ -28,8 +71,30 @@ class BrowserControl:
         self.teardown()
 
     def login(self, username: str = Config.ZOHO_USERNAME, password: str = Config.ZOHO_PASSWORD):
-        self.page.fill(LoginPageSelectors.username_input, username)
-        self.page.click(LoginPageSelectors.next_button)
-        self.page.fill(LoginPageSelectors.username_input, password)
-        self.page.click(LoginPageSelectors.next_button)
-        self.page.wait_for_url(DashboardPageSelectors.url)
+        self.login_pg.username_input.fill(username)
+        self.login_pg.next_button.click()
+        if self.login_pg.is_daily_limit_reached():
+            # NOTE: Zoho have daily limit in 20 login events.
+            msg = "Daily sign-in limit reached. Breaking the task."
+            logger.exception(msg)
+            raise EnvironmentError(msg)
+        self.login_pg.password_input.fill(password)
+        self.login_pg.next_button.click()
+        if self.login_pg.is_daily_limit_warning():
+            self.login_pg.understand_button.click()
+        self.dashboard_pg.wait_for_loading()
+        self.dashboard_pg.att_button.wait_for(state="visible")
+
+    def switch_attendancy(self):
+        logger.debug(self.context.cookies())
+        self.dashboard_pg.att_button.click()
+        self.context.grant_permissions(["geolocation"])
+
+    def get_att_state(self):
+        return self.dashboard_pg.att_button.inner_text()
+
+
+bc = BrowserControl()
+bc.login()
+print(bc.get_att_state())
+bc.switch_attendancy()
