360a287dbfac11f8bd6a171c89328f2c8a6e261f
nickosh <nikolay.shishov@gmail.com>
2024-08-22 15:07:09 +0900

    main.py file beautification

--- src/attctrl/main.py
+++ src/attctrl/main.py
@@ -1,1 +1,16 @@
+"""
+################################################################################
+#                                                                              #
+#                              [attctrl_zoho]                                  #
+#                       Zoho People Attendance Control                         #
+#                                                                              #
+#  File:     main.py                                                           #
+#  Purpose:  Entry point for the attctrl_zoho application                      #
+#                                                                              #
+#  Author:   Nikolay Shishov [@nickosh]                                        #
+#  Date:     2024/08/22                                                        #
+#                                                                              #
+################################################################################
+"""
+
 from attctrl.api import app
